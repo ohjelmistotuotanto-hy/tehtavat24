f2d4e1f8354f327af629372bdf5e6ecae1576ccc
Matti Luukkainen <mluukkai@iki.fi>
2024-10-24 20:14:45 +0200

    moar fixes

--- viikko3/web-login/src/AppLibrary.py
+++ viikko3/web-login/src/AppLibrary.py
@@ -5,8 +5,6 @@ class AppLibrary:
     def __init__(self):
         self._base_url = "http://localhost:5001"
 
-        self.reset_application()
-
     def reset_application(self):
         requests.post(f"{self._base_url}/tests/reset")
 
